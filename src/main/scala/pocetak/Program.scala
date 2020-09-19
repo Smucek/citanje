0174de112362d56119d2fbbd1ce45dae9bd4f127
Smucek <ajdindjakusic@gmail.com>
2020-09-19 13:51:17 +0200

    Sekvencijalno citanje

---
+++ src/main/scala/pocetak/Program.scala
@@ -0,0 +1,15 @@
+package pocetak
+
+import scala.io.Source
+
+object Program {
+
+  def main(args: Array[String]) = {
+
+    //      val start = System.currentTimeMillis()
+    for (i <- 1 until 11) {
+      (Source.fromFile(("Zadaca2File" + i.toString + ".txt")).getLines().toSeq).foreach(println)
+    }
+
+  }
+}
