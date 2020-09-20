ba5317ad673fa88554b6c4710341c0e8974c7ddf
Smucek <ajdindjakusic@gmail.com>
2020-09-20 19:29:21 +0200

    Poredjenje sekvencijalnog i paralelnog citanja fajlova.

--- src/main/scala/pocetak/Program.scala
+++ src/main/scala/pocetak/Program.scala
@@ -1,11 +1,38 @@
 package pocetak
 
+import scala.concurrent.Future
+import scala.io.Source
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
 object Program extends SekvencijalnoCitanje {
 
+  def futureCitanje(): Future[Unit] = Future {
+    for (i <- 1 until 11) {
+      val izvorniFajlovi = Source.fromFile("Zadaca2File" + i.toString + ".txt")
+      val linijeFajla: Seq[String] = izvorniFajlovi.getLines().toSeq
+
+      linijeFajla.foreach { linija =>
+        println(linija)
+    }
+  }
+}
+
   def main(args: Array[String]) = {
+
+
+    futureCitanje().onComplete {
+      case Success(value) => futureCitanje()
+      case Failure(exception) => println(s"Greska + ${exception.getMessage}")
+    }
+    val startP = System.currentTimeMillis()
+    Thread.sleep(3000)
+    val a = (s"Paralelno citanje 10 txt fajlova ocitano u ${System.currentTimeMillis() - startP - 3000} ms")
 
     val start = System.currentTimeMillis()
     sekvencijalnoCitanje()
     println(s"Sekvencijalno citanje 10 txt fajlova ocitano u ${System.currentTimeMillis() - start} ms")
+    println(a)
+
   }
 }
